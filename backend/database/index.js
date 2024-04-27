6fa8cfa5aa883873d5dcb7e21ac1aca3047b4bbf
Umair <umairshakeelkhan@gmail.com>
2024-04-27 17:47:03 +0500

    a change

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const {MONGODB_CONNECTION_STRING} = require('../config/index');
 
-// const connectionString = "mongodb+srv://bugAuthor:coinbounce@cluster0.knnvdfd.mongodb.net/coin-bounce?retryWrites=true&w=majority&appName=Cluster0";
-
 const dbConnect = async () => {
     try {
         mongoose.set('strictQuery', false);
